916cdb0268328ba1bc33e2edf445456cb34776f4
Dustin Stender <dustinpstender@gmail.com>
2021-08-16 13:03:53 +0200

    added controllers

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,13 @@
+class BookingsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/destroy'
+  get 'bookings/show'
+  get 'offers/index'
+  get 'offers/show'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers, only: [:index, :show] do

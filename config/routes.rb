64336185c59a13306c027409777a5c690a94c9cf
Dustin Stender <dustinpstender@gmail.com>
2021-08-16 12:57:19 +0200

    created the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :offers, only: [:index, :show]
+  resources :offers, only: [:index, :show] do
+    resources :bookings, only: [:create, :show, :new, :destroy]
+  end
 end

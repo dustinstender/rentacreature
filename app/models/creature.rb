7f8831271efbdcce61d17fce65ec8ee837425d1b
marly teixeira <teixeiramg.marly@gmail.com>
2021-08-19 14:53:27 +0200

    adding the new creature + dynamic cloudinary

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,6 +1,6 @@
 class Creature < ApplicationRecord
+  has_one_attached :profile_pic
   belongs_to :user
-  has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'creatures/shsp', to: 'creatures#search_species'
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :creatures, only: [:index, :show] do
+  resources :creatures, only: [:index, :show, :new, :create] do
     resources :bookings, only: [:create, :show, :new]
   end
   resources :bookings, only: [:index, :destroy]

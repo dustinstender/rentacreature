104c1eb6cf6f5bc08592a6dd29e0538c6127446b
Dustin Stender <dustinpstender@gmail.com>
2021-08-20 14:12:02 +0200

    minor changes to make the html cleaner

--- app/javascript/plugins/listener_date.js
+++ app/javascript/plugins/listener_date.js
@@ -11,7 +11,7 @@ const computPrice = () => {
       const diffDays = (end_date_value - start_date_value) / (1000 * 3600 * 24);
       const creaturePrice = parseInt(document.querySelector("#creature_price").innerHTML);
       const price = creaturePrice * diffDays
-      document.querySelector("#total_price").innerHTML = `Your experience will cost ${price} $.`
+      document.querySelector("#total_price").innerHTML = `Total cost is ${price}$.`
       document.querySelector("#duration").innerHTML = `Your experience will last ${diffDays} days.`
 
     }

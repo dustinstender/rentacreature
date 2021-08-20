2e4f3d259e46fe40690b2295a9e3e1e4718bcde0
Dustin Stender <dustinpstender@gmail.com>
2021-08-20 12:45:20 +0200

    something with nav of course

--- db/schema.rb
+++ db/schema.rb
@@ -67,10 +67,11 @@ ActiveRecord::Schema.define(version: 2021_08_19_144610) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.string "description"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
